e3392a5a7e1c0044de638f2d2127dff99b22ddde
erdemkarakoylu <erdemk@protonmail.com>
2023-04-03 12:43:45 -0400

    added cleaning functionality

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -4,6 +4,7 @@ Download the raw data set, clean it, export the resulting artifact.
 """
 import argparse
 import logging
+import pandas as pd
 import wandb
 
 
@@ -19,59 +20,76 @@ def go(args):
     # Download input artifact. This will also log that this script is using this
     # particular version of the artifact
     # artifact_local_path = run.use_artifact(args.input_artifact).file()
-
     ######################
     # YOUR CODE HERE     #
     ######################
+    logger.info("Downloading artifact.")
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+    df = pd.read_csv = pd.read_csv(artifact_local_path)
 
+    logger.info(f"Selecting data with price between {args.min_price:.2f} and {args.max_price:.2f}.")
+    idx = df['price'].between(args.min_price, args.max_price)
+    df = df[idx].copy()
+    
+    output_filename = "clean_sample.csv"
+
+    logger.info(f"Saving artifact as {output_filename}.")
+    df.to_csv(output_filename, index=False)
+
+    logger.info("Logging cleaned data.")
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description
+    )
+    artifact.add_file(output_filename)
+    run.log_artifact(artifact)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Basic cleaning step.")
 
-
     parser.add_argument(
         "--input_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=str,
+        help="Full input artifact name.",
         required=True
-    )
+        )
 
     parser.add_argument(
         "--output_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=str,
+        help="Full output artifact name.",
         required=True
     )
 
     parser.add_argument(
         "--output_type", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=str,
+        help="Output artifact type.",
         required=True
     )
 
     parser.add_argument(
         "--output_description", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=str,
+        help="Description of the output artifact.",
         required=True
     )
 
     parser.add_argument(
         "--min_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=float,
+        help="Minimum price to include in data.",
         required=True
     )
 
     parser.add_argument(
         "--max_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
+        type=float,
+        help="Maximum price to include in data.",
         required=True
     )
-
 
     args = parser.parse_args()
 
